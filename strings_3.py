3a5cc05864b709f5426aea5e6899e76a7575d7e1
Darshan <darshandarshu7520@gmail.com>
2025-06-08 21:33:04 +0530

    string functions

--- strings_3.py
+++ strings_3.py
@@ -9,8 +9,25 @@ d="college"
 d_len=len(d)
 print(d_len)  #return the lenght of the string
 
+
+#string functions
+
 e="window"
 e_short=e[0:4]  #start from index 0 all but exclude 4 index
 print(e_short)
+
+print(e.endswith("dow"))
+print(e.capitalize())
+print(e.startswith("Wind"))
+print(e.title())
+
+#escape sequence
+a="hello \n world" 
+b="hello \t world"
+c=("you are a \" good \" guys" )
+print (a)
+print(b)
+print(c)
+
 
  
