d6086f29415bae7f78f1103be8f0ac45a13790bf
Darshan <darshandarshu7520@gmail.com>
2025-06-08 21:00:30 +0530

    string operations

---
+++ strings_3.py
@@ -0,0 +1,16 @@
+#we can write string in three ways
+
+a='in single quote'
+b="in double quote"
+c='''in triple quote'''
+
+#string slicing
+d="college"
+d_len=len(d)
+print(d_len)  #return the lenght of the string
+
+e="window"
+e_short=e[0:4]  #start from index 0 all but exclude 4 index
+print(e_short)
+
+ 
