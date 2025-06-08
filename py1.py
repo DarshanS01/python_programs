85a23486c98b55a68e3f7f46185aea9c8f366097
Darshan <darshandarshu7520@gmail.com>
2025-06-08 12:24:07 +0530

    printing and comment

--- py1.py
+++ py1.py
@@ -1,1 +1,13 @@
-print("hello");
+#simple printing statement
+print("this is a first statement")
+
+#this is comment line and single line comment
+# this line is ignored by the interpriter
+
+
+#for multiline comment we use ''' '''
+print('''
+      hello
+      hii
+      byee'''
+)
