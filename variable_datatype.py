e245f872d7da76892f74171c2c4b59bc5534cd3f
Darshan <darshandarshu7520@gmail.com>
2025-06-08 13:01:55 +0530

    variables and datatype

---
+++ variable_datatype.py
@@ -0,0 +1,53 @@
+#variables
+a=10       #this is integer variable
+b="darsh"  #this is string variable
+c=2.04     #this is floating point variable
+
+
+#datatypes
+a=15
+print(type(a)) #this is integer datatype
+
+b=3.14
+print(type(b)) #this is floating point datatype
+
+c="string"
+print(type(c)) #this is string datatype
+
+d=True
+print(type(d)) #this is boolean datatype
+
+f=None
+print(type(f)) #this is None datatype
+
+
+#operators in python
+
+#i.arithmetic operator
+a=10
+b=2
+print("addition of two num is:",a+b)
+print("subtraction of two num is:",a-b)
+print("multiplication of two num is:",a*b)
+print("division of two num is:",a/b)
+
+#ii.assignment operator
+a=8   #value 8 is assigning to variable a
+a+=5  # 5 is added to 8 and result is asign to a
+a-=5  # 8 is subtracted by 5 and result is asign to a
+a*=5  # 8 is multiple by 5 and result is asign to a
+a/=2  # 8 is divided by 2 and result is asign to a
+
+#iii.comparision operator
+a=10<5
+print(a) #it give boolean value Flase
+b=10>5
+print(b) #it give boolean value True
+c=10==5
+print(c) #it give boolean value Flase
+d=10!=5
+print(d) #it give boolean value True
+
+#iv.logicla operators
+
+
