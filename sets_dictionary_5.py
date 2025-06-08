c3cd96636873192580edc191f945fe1dac5d9a01
Darshan <darshandarshu7520@gmail.com>
2025-06-08 23:34:55 +0530

    dictionary

---
+++ sets_dictionary_5.py
@@ -0,0 +1,12 @@
+ #dictionary
+ 
+marks={" bharatha":88,
+       "jagu":55,
+       "pappy":91    
+ }
+print(marks,type(marks))
+
+print(marks.items())
+print(marks.update({"jaguu":87,"ummi":100}))
+print(marks.keys())
+print(marks.values())
