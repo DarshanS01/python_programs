7ccb7f1fae9045d71da2fa52b978438ed2676b12
Darshan <darshandarshu7520@gmail.com>
2025-06-09 21:25:50 +0530

    added

--- sets_dictionary_5.py
+++ sets_dictionary_5.py
@@ -10,3 +10,14 @@ print(marks.items())
 print(marks.update({"jaguu":87,"ummi":100}))
 print(marks.keys())
 print(marks.values())
+
+
+
+  #sets
+s=set()   #empty set
+  
+s1={1,3, 21,3,4}
+s2={21,6,8,3,2}
+
+print(s1.union(s2))
+print(s1.intersection(s2))
