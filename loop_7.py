f7fea128afcadd0c543eab74c738be11a287aaa4
Darshan <darshandarshu7520@gmail.com>
2025-06-13 23:32:48 +0530

    loop statemenet

--- loop_7.py
+++ loop_7.py
@@ -1,4 +1,25 @@
-#looping statement
+# while loop statement
 
 for i in range(0,101):
     print(i)
+    
+i=1
+while i<5:
+    print(i)
+    i=i+1
+    
+    
+#for loop statemenet
+for i in range(9):
+    print(i)
+    
+s="darshan"
+for i in s:
+    print(i)
+    
+#loop with else
+t=(1,True,"op",1.2)
+for f in t:
+    print(f)
+else:
+    print("done")
