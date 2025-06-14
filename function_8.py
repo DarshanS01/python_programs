d6883e2cf05e887e36c7265d79ed63afcd8c46ec
Darshan <darshandarshu7520@gmail.com>
2025-06-14 16:38:43 +0530

    calculator added

--- function_8.py
+++ function_8.py
@@ -1,7 +1,24 @@
-def avg():
-    a=int(input("enetr the number"))
-    b=int(input("enetr the number"))
-    c=int(input("enetr the number"))
-    average=(a+b+c)/3
-    print(average)
-avg()
+#function definition
+# def avg():
+#     a=int(input("enetr the number"))
+#     b=int(input("enetr the number"))
+#     c=int(input("enetr the number"))
+#     average=(a+b+c)/3
+#     print(average)
+# avg()
+
+
+def calculator(num1,op,num2):
+    if op=="+":
+        print(num1+num2)
+    if op=="/":
+        prin(num1/num2)
+    if op=="*":
+        print(num1*num2)
+    if op=="-":
+        print(num1-num2) 
+num1=int(input("enter the number"))
+num2=int(input("enter the number"))
+op=(input("enter the number"))
+res=calculator(num1,op,num2)
+print(res)
