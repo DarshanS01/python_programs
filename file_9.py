9b238a8a4362020b7df11ae8372ca500abaa663c
Darshan <darshandarshu7520@gmail.com>
2025-06-21 14:03:45 +0530

    new prgrm

--- file_9.py
+++ file_9.py
@@ -1,4 +1,11 @@
-f=open("file.txt")
-data=f.read()
-print(data)
-f.close()
+# f=open("file.txt")
+# data=f.read()
+# print(data)
+# f.close()
+d="hello bye"
+with open("file.txt","w")as f:
+    f.write(d)
+    
+    
+with open("file.txt")as f:
+    f.read()
