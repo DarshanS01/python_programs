88f40e19d4ed2b12d34f8d09e0c901fa97c25a95
Darshan <darshandarshu7520@gmail.com>
2025-06-21 20:09:36 +0530

    class is updated

--- class.py
+++ class.py
@@ -1,8 +1,19 @@
 class emp:
-    name="darshan"
     age=20
+    course="bca "
     salary=800000
     
-
 darsh=emp()
-print(darsh.name,darsh.age,darsh.salary)
+darsh.name="darshan"
+print(darsh.name,darsh.age,darsh.salary,darsh.course)
+
+
+
+ng=emp()
+ng.name="nag" 
+ng.course="bcom"
+print(ng.name,ng.age,ng.salary,ng.course)
+
+print(type(darsh))
+
+
